7883057a0273be1bceb54f792bf02184d27de528
pixeltot <pixeltot@github.com>
2016-04-12 23:02:09 -0700

    added recent and finished to Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ActiveRecord::Base
+
+	scope :finished, -> { where.not(finished_on: nil) }
+
+
+	#scope :recent, -> {where('finished_on > ?', 2.days.ago)}
+
+	# scope can also be written as a class method as in the example below
+	# using self. makes it a class method
+	# query methods as class methods
+
+	def self.recent
+		where('finished_on > ?', 2.days.ago)
+	end
+
 	def finished?
 		finished_on.present?
 	end
