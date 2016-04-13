e6cd7684c72088a98c61117f30060a698ef4c5de
pixeltot <pixeltot@github.com>
2016-04-12 21:51:17 -0700

    add books.

---
+++ app/models/book.rb
@@ -0,0 +1,6 @@
+class Book < ActiveRecord::Base
+	def finished?
+		finished_on.present?
+	end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create!([{
+	title: "Hyperion",
+	author: "Dan Simmons",
+	description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		amazon_id: "0553283685",
+		rating: 5,
+		finished_on: 10.days.ago
+},
+{
+	title: "Jony Ive: The Genius Behind Apple's Greatest Products",
+	author: "Leander Kahney",
+	description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+	amazon_id: "159184617X",
+	rating: 4,
+	finished_on: 1.day.ago
+},
+{
+	title: "Mindstorms",
+	author: "Stupid Mother",
+	description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+	amazon_id: "0465046746",
+	rating:3,
+	finished_on: nil
+}])
+
+p "Created #{Book.count} books"
